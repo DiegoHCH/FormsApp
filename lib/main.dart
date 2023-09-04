a56d9c88b1b4c306d3b5dcc7359ad153d16ec206
Diego Fernando Hoyos Chacon <diego.hoyos@wiedii.co>
2023-09-04 14:42:34 -0500

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:forms_app/config/router/app_router.dart';
+import 'package:forms_app/config/theme/app_theme.dart';
+
+void main() {
+   runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      title: 'FormsApp',
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme().getTheme(),
+      routerConfig: appRouter,
+    );
+  }
+}
